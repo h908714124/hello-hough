77b2ebc1c112213d760d06c67cea0ba4de28629f
h908714124 <kraftdurchblumen@gmx.de>
2018-04-18 11:28:56 +0200

    flatten array

--- hough.py
+++ hough.py
@@ -1,32 +1,37 @@
+from __future__ import print_function
+
 import sys
 
 import cv2
 import numpy as np
+import tensorflow as tf
 
 from hello.arg_parser import ArgParser
 from hello.im_debug import show
 
+tf.enable_eager_execution(config=None, device_policy=None)
+
 
 def main(flags):
-  
   img = cv2.imread(flags.image)
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   edges = cv2.Canny(gray, 50, 150, apertureSize=3)
   
   lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+  lines = tf.reshape(lines, [-1, 2]).numpy()
   
-  for line in lines:
-    for rho, theta in line:
-      a = np.cos(theta)
-      b = np.sin(theta)
-      x0 = a * rho
-      y0 = b * rho
-      x1 = int(x0 + 1000 * (-b))
-      y1 = int(y0 + 1000 * (a))
-      x2 = int(x0 - 1000 * (-b))
-      y2 = int(y0 - 1000 * (a))
-      
-      cv2.line(img, (x1, y1), (x2, y2), (245, 0, 0), 2)
+  for rho, theta in lines:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    factor = 500
+    x1 = int(x0 + factor * (-b))
+    y1 = int(y0 + factor * (a))
+    x2 = int(x0 - factor * (-b))
+    y2 = int(y0 - factor * (a))
+    
+    cv2.line(img, (x1, y1), (x2, y2), (245, 0, 0), 2)
   
   show(img)
 
