fbe4be7484d148fde4d69b7fd119d5b5eeab35f8
h908714124 <kraftdurchblumen@gmx.de>
2018-04-19 18:15:16 +0200

    stash

--- hough.py
+++ hough.py
@@ -6,6 +6,8 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
+import PIL.Image
+
 from hello.arg_parser import ArgParser
 from hello.im_debug import show
 
@@ -44,31 +46,53 @@ def remove_duplicate_lines(max, lines):
 
 def remove_outliers(lines):
   diff = np.diff(lines)
-  mean = np.mean(diff)
-  deviations = [np.square(mean - x) for x in diff]
-  cutoff = np.max(deviations) / 2
+  mean_diff = np.mean(diff)
+  deviations = [np.square(mean_diff - x) for x in diff]
+  cutoff = np.max(deviations) * 0.375
   result = np.zeros(len(lines))
   result_pos = 0
   for index, line in enumerate(lines):
     if index == 0:
-      d = np.square(lines[index + 1] - line - mean)
+      d = np.square(lines[index + 1] - line - mean_diff)
       if d < cutoff:
         result[result_pos] = line
         result_pos += 1
     elif index == len(lines) - 1:
-      d = np.square(lines[index - 1] - line - mean)
+      d = np.square(lines[index - 1] - line - mean_diff)
       if d < cutoff:
         result[result_pos] = line
         result_pos += 1
     else:
-      left = np.square(line - lines[index - 1] - mean)
-      right = np.square(lines[index + 1] - line - mean)
+      left = np.square(line - lines[index - 1] - mean_diff)
+      right = np.square(lines[index + 1] - line - mean_diff)
       if left < cutoff or right < cutoff:
         result[result_pos] = line
         result_pos += 1
   result = result[0:result_pos]
   if len(result) != grid_size:
     raise Exception("Can't find the grid!")
+  return result
+
+
+# 4x4x3 matrix: A = np.arange(48).reshape(-1,3,4)
+def get_tiles(img, horizontal_lines, vertical_lines):
+  hdiff = np.diff(horizontal_lines)
+  mean_hdiff = np.mean(hdiff)
+  vdiff = np.diff(vertical_lines)
+  mean_vdiff = np.mean(vdiff)
+  result = np.zeros([361, 32, 32], dtype=np.float32)
+  result_pos = 0
+  for i_h, hline in enumerate(horizontal_lines):
+    for i_v, vline in enumerate(vertical_lines):
+      y1 = int(hline - mean_hdiff) / 2
+      y2 = int(hline + mean_hdiff) / 2
+      x1 = int(vline - mean_vdiff) / 2
+      x2 = int(vline + mean_vdiff) / 2
+      full_size_tile = img[y1:y2, x1:x2]
+      pil_image = PIL.Image.fromarray(full_size_tile)
+      resized = pil_image.resize([32, 32], PIL.Image.BILINEAR)
+      result[result_pos] = np.asarray(resized, dtype=np.float32)
+      result_pos += 1
   return result
 
 
@@ -108,7 +132,10 @@ def main(flags):
   horizontal_lines = remove_duplicate_lines(np.shape(img)[1], horizontal_lines)
   vertical_lines = remove_outliers(vertical_lines)
   horizontal_lines = remove_outliers(horizontal_lines)
-  
+
+  tiles = get_tiles(img, horizontal_lines, vertical_lines)
+  show(tiles[0])
+
   for y in horizontal_lines:
     add_horizontal_line(img, int(y))
   
